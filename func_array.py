70ade96d387e7dbf385fdbf7ce698c3b05d950c6
LeonardoIalongo <leonardo.ialongo@gmail.com>
2022-10-03 12:12:33 +0200

    Created skeleton of functional array class.

---
+++ func_array.py
@@ -0,0 +1,46 @@
+from numba import njit
+import numpy as np
+
+
+class array(object):
+    """The functional array is an array whose elements are computed on demand 
+    rather than stored. 
+
+    This allows for faster computation of properties of measures computed by 
+    iterating over its elements. It also allows to have arrays that would not 
+    fit in memory but can be handled as if they were.
+    """
+    def __init__(self, shape, fun, *args):
+        """Return a FuncArray object. 
+
+        :param fun: function that computes each element of the array.
+        :type T: function.
+        :param args: arguments necessary for fun to be computed.
+        """
+
+        self.fun = fun
+        self.args = args
+        self.shape = shape
+        self.ndim = len(shape)
+
+    def __iter__(self):
+        for r in range(self.shape[0]):
+            yield self[r]
+
+    def __getitem__(self, index):
+        if not isinstance(index, int):
+            for s in index:
+                if isinstance(s, slice):
+                    raise ValueError('Slicing not yet supported.')
+            return self.fun(*index, *self.args)
+
+    def sum(self):
+        return self._sum(self.fun, self.shape, self.args)
+
+    @staticmethod
+    @njit
+    def _sum(fun, shape, args):
+        res = 0
+        for index in np.ndindex(shape):
+            res += fun(*index, *args)
+        return res
